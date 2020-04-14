77e2d5b8edf0b38081a3dc299b2a2e71b6dbeb82
Maximiliano <maximiliano.rod.ra@gmail.com>
2020-04-14 11:28:48 -0400

    Updated 05_brazil_geocodes from Meyhar: Pending review

--- DataWork/twitter/05_geocodes/05_brazil_geocodes.R
+++ DataWork/twitter/05_geocodes/05_brazil_geocodes.R
@@ -1,28 +1,54 @@
-library(scales)
-library(ggplot2)
-library(dplyr)
-library(doBy)
-library(geojsonsf)
-library(sf)
-library(stringr)
-library(broom)
-library(ggmap)
-library(lubridate)
-library(ggmap)
-#devtools::install_github("fresques/ggmap")
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Load packages ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+if (!require("pacman")) install.packages("pacman")
 
-# Keys -------------------------------------------------------------------------
+pacman::p_load(gtrendsR, tidyverse, parallel, pbmcapply, ggplot2, scales, doBy,
+               jsonlite, stringr, raster, stringi, lubridate, purrr, lexiconPT,
+               tidytext, quanteda, qdap, SentimentAnalysis, sentimentr, ggmap,
+               tm, tokenizers, wordcloud, ggwordcloud, ggpubr, hrbrthemes,
+               geojsonsf, sf, broom)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Keys -----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-register_google(key = "AIzaSyB6AkkAWXqgJYFdDbWxcS4aZyNtqSnooJo")
-has_google_key()
+register_google(key = "YOUR KEY")
+has_goog_key() # Should return TRUE
 
-# get the input data
+# Temp directory
+temp_dir <- file.path(dropbox_file_path, "Data", "twitter", "RawData", "geo")
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Load data ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 location_brazil <- readRDS(file.path(dropbox_file_path, "Data", "twitter", "FinalData", "brazil_tweets", "rds", "brazil_tweets_appended_clean.Rds"))
 
-# get the address list, and append "Ireland" to the end to increase accuracy 
-# (change or remove this if your address already include a country etc.)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Clean the dataset ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Remove duplicates 
+location_brazil <- location_brazil[!duplicated(location_brazil$location), ]
+
+# To lower case
+location_brazil <- location_brazil %>% 
+    mutate(location = tolower(location)) %>% 
+    filter(!is.na(location))
+
+# Clean locations that include leading white space, emojis and include brasil
+# Note: This needs to be cleaned: There are some locations that contain strings that will create conflict in the query
+location_brazil <- location_brazil %>% 
+    mutate(location = gsub("[^[:alnum:][:space:]]","", location),
+           location = gsub("brasil|brazil", "", location),
+           location = gsub("^\\s+|\\s+$", "", location),
+           location = paste0(location, ", brasil"))
+
+# Define a range of address just to try to loop : This should be removed if it is working
+location_brazil<- location_brazil[1:300,]
+
+# Get only the addresses
 addresses = location_brazil$location
-addresses = paste0(addresses)
 
 #define a function that will process googles server responses for us.
 getGeoDetails <- function(address){   
@@ -54,18 +80,27 @@ getGeoDetails <- function(address){
     answer$formatted_address <- geo_reply$results[[1]]$formatted_address
     return(answer)
 }
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# GGMap Address extracton ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 #initialise a dataframe to hold the results
 geocoded <- data.frame()
+
 # find out where to start in the address list (if the script was interrupted before):
 startindex <- 1
+
 #if a temp file exists - load it up and count the rows!
-tempfilename <- paste0(dropbox_file_path, '_temp_geocoded.rds')
+tempfilename <- file.path(temp_dir, "temp_geocoded.rds")
+
 if (file.exists(tempfilename)){
     print("Found temp file - resuming from index:")
     geocoded <- readRDS(tempfilename)
     startindex <- nrow(geocoded)
     print(startindex)
 }
+
 # Start the geocoding process - address by address. geocode() function takes care of query speed limit.
 for (ii in seq(startindex, length(addresses))){
     print(paste("Working on index", ii, "of", length(addresses)))
@@ -78,10 +113,4 @@ for (ii in seq(startindex, length(addresses))){
     #save temporary results as we are going along
     saveRDS(geocoded, tempfilename)
 }
-#now we add the latitude and longitude to the main data
-data$lat <- geocoded$lat
-data$long <- geocoded$long
-data$accuracy <- geocoded$accuracy
-#finally write it all to the output files
-saveRDS(data, paste0("../data/", infile ,"_geocoded.rds"))
-write.table(data, file=paste0("../data/", infile ,"_geocoded.csv"), sep=",", row.names=FALSE)
+
