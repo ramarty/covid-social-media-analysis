48a751c6527ae0bdf46e83310c45c1a439df5eab
manuelmrm <manuelmrm@users.noreply.github.com>
2020-04-18 10:55:20 -0400

    Update 02_clean_data.R

--- DataWork/google_trends/02_clean_data.R
+++ DataWork/google_trends/02_clean_data.R
@@ -6,14 +6,12 @@ trends_df_ms <- readRDS(file.path(dropbox_file_path, "Data/google_trends/RawData
 admin_data <- read.dta13(file.path(dropbox_file_path, "Data/brazil_admin_data/admindata.dta"))
 geo_data <- readRDS(file.path(dropbox_file_path, "Data/GADM/RawData/gadm36_BRA_1_sp.rds"))
 state_pop_data <- read.csv(file.path(dropbox_file_path, "Data/city_population/FinalData/brazil_state_pop.csv"))
+trends_df_extra <- readRDS(file.path(dropbox_file_path, "Data/google_trends/RawData/brazil_extract_extra_words.Rds"))
 
 # Merge trends_df with trends_df_ms
 trends_df <- 
   trends_df %>% 
   bind_rows(trends_df_ms)
-
-trends_df %>% 
-  count(name, date) %>% view() 
 
 # Clean Variables --------------------------------------------------------------
 
@@ -25,6 +23,25 @@ trends_df$hits <- trends_df$hits %>% as.numeric()
 #### Date
 # Convert into date format
 trends_df$date <- trends_df$date %>% as.Date()
+
+
+# Clean dataset of extra words
+trends_df_extra <- 
+  trends_df_extra %>% 
+  mutate(
+    date = as.Date(date), 
+    hits = if_else(hits < 1, "0.5", hits), 
+    hits = as.numeric(hits)
+  )
+
+trends_df_extra <- 
+  trends_df_extra %>% 
+  mutate(state = state_en = stringi::stri_trans_general(state, "Latin-ASCII") %>% str_to_upper()) 
+
+# Add extra words to the dataset
+
+trends_df %>% 
+  bind_rows(trends_df_extra) ## NEEDS TO BE REVIEWED
 
 # Merge admin cases with deaths and pop data---------------------------------------------------------
 
