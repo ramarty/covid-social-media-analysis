c7f7e25741e0d3c33e313fc2f5713694b8ab28f4
Rob Marty <ramarty@email.wm.edu>
2021-03-30 18:22:44 -0400

    up

--- DataWork/google_trends/paper_analysis/vaccine_maps.R
+++ DataWork/google_trends/paper_analysis/vaccine_maps.R
@@ -54,13 +54,13 @@ google_df_dna <- google_df %>%
 
 google_df_beast <- google_df %>%
   filter(geo %in% "US",
-         keyword %in% "covid vaccine cause infertility") %>%
+         keyword %in% "covid vaccine mark of beast") %>%
   mutate(month = time_span %>% substring(1,10) %>% ymd()) %>%
   left_join(cases_df, by = c("location", "month")) 
 
 google_df_inf <- google_df %>%
   filter(geo %in% "US",
-         keyword %in% "covid vaccine cause infertility") %>%
+         keyword %in% "can covid vaccine cause infertility") %>%
   mutate(month = time_span %>% substring(1,10) %>% ymd()) %>%
   left_join(cases_df, by = c("location", "month")) 
 
@@ -68,13 +68,7 @@ states <- get_urbn_map("states", sf = T)
 states <- states %>%
   dplyr::rename(location = state_name)
 
-for(month in c("2020-03-01",
-               "2020-04-01",
-               "2020-05-01",
-               "2020-06-01",
-               "2020-07-01",
-               "2020-08-01",
-               "2020-09-01",
+for(month in c("2020-09-01",
                "2020-10-01",
                "2020-11-01",
                "2020-12-01",
@@ -84,24 +78,60 @@ for(month in c("2020-03-01",
   month_date <- as.Date(month)
   
   google_df_dna_i <- google_df_dna %>%
-    filter(month %in% !!month_date)
+    filter(month %in% !!month_date) %>%
+    dplyr::select(hits, location) %>%
+    dplyr::rename(hits_dna = hits)
   
+  google_df_beast_i <- google_df_beast %>%
+    filter(month %in% !!month_date) %>%
+    dplyr::select(hits, location) %>%
+    dplyr::rename(hits_beast = hits)
   
-  states$location %in% google_df_i$location
-  states_i <- merge(states, google_df_i)
+  google_df_inf_i <- google_df_inf %>%
+    filter(month %in% !!month_date) %>%
+    dplyr::select(hits, location) %>%
+    dplyr::rename(hits_inf = hits)
   
-  p <- states_i %>%
+  states_i <- merge(states,   google_df_dna_i,   by = "location", all.x=T, all.y=F)
+  states_i <- merge(states_i, google_df_beast_i, by = "location", all.x=T, all.y=F)
+  states_i <- merge(states_i, google_df_inf_i,   by = "location", all.x=T, all.y=F)
+  
+  p_dna <- states_i %>%
     ggplot() +
-    geom_sf(aes(fill = hits)) +
+    geom_sf(aes(fill = hits_dna)) +
     scale_fill_viridis(na.value = "gray50") +
     theme_void() +
-    labs(fill = "Cases\nper capita",
-         title = month) +
+    labs(fill = "Search Interest") +
     theme(legend.position = "bottom",
           plot.title = element_text(face = "bold", hjust = 0.5))
-
+  
+  p_beast <- states_i %>%
+    ggplot() +
+    geom_sf(aes(fill = hits_beast)) +
+    scale_fill_viridis(na.value = "gray50") +
+    theme_void() +
+    labs(fill = "Search Interest") +
+    theme(legend.position = "bottom",
+          plot.title = element_text(face = "bold", hjust = 0.5))
+  
+  p_inf <- states_i %>%
+    ggplot() +
+    geom_sf(aes(fill = hits_inf)) +
+    scale_fill_viridis(na.value = "gray50") +
+    theme_void() +
+    labs(fill = "Search Interest") +
+    theme(legend.position = "bottom",
+          plot.title = element_text(face = "bold", hjust = 0.5))
+  
+  p <- ggarrange(p_dna, p_beast, p_inf,
+                 nrow = 1,
+                 common.legend = T)
+  p <- annotate_figure(p,
+      top = text_grob(month, color = "black", face = "bold", size = 14)
+    )
+  
   ggsave(p, filename = file.path("~/Desktop",
-                                 folder_i,
+                                 "vxmap",
                                  paste0(as.character(month), ".png")),
          height = 4, width = 12)
 }
