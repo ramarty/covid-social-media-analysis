403e7950977876fc0690bf20f6a3b1697c66a624
Rob Marty <ramarty@email.wm.edu>
2021-08-31 23:02:35 -0400

    did

--- DataWork/google_trends/paper_analysis/03a_lockdown_did.R
+++ DataWork/google_trends/paper_analysis/03a_lockdown_did.R
@@ -1,5 +1,19 @@
 # Lockdown Difference-in-Difference Analysis
 
+lm_post_confint_tidy <- function(lm){
+  
+  lm_confint <- confint(lm) %>% 
+    as.data.frame
+  names(lm_confint) <- c("p025", "p975")
+  lm_confint$b <- (lm_confint$p025 + lm_confint$p975)/2
+  lm_confint$variable <- row.names(lm_confint)
+  
+  lm_confint$tvalue <- summary(lm)$coefficients[,3] %>% as.vector()
+  lm_confint$pvalue <- summary(lm)$coefficients[,4] %>% as.vector()
+  
+  return(lm_confint)
+}
+
 # Load / Prep Data -------------------------------------------------------------
 gtrends_df <- readRDS(file.path(dropbox_file_path, "Data", "google_trends", "FinalData",
                                 "gtrends_full_timeseries", "gtrends_otherdata_varclean.Rds"))
@@ -10,213 +24,45 @@ gtrends_df <- gtrends_df %>%
                   days_since_lockdown_min_yearcurrent_post*year2020)
 
 # Days since lockdown, all years -----------------------------------------------
-gtrends_df_i <- gtrends_df %>%
-  dplyr::filter(keyword_en %in% "boredom") 
+keywords_en_use <- c("social distance", "stay at home", "boredom", "anxiety", "suicide",
+                     "insomnia", "social isolation", "loneliness", "divorce",
+                     "panic attack",
+                     "fever",
+                     "worried health", "hysteria", "overwhelmed", "anxiety symptoms",
+                     "anxiety attack", "symptoms of pannic attack",
+                     "depressed", "lonely", "suicidal", "abuse",
+                     "therapist near me", "online therapist",
+                     "deep breathing", "body scan meditation",
+                     "unemployment", "unemployment insurance")
 
-lm(hits_ma7 ~ year2020 + 
-     days_since_lockdown_min_yearcurrent_post + 
-     days_since_lockdown_min_yearcurrent_post_X_year2020, data = gtrends_df_i) %>%
-  summary()
-
-# y ~ x1 + x2 | f1 + f2 | (Q|W ~ x3+x4) | clu1 + clu2 
-felm(hits_ma7 ~ year2020 + 
-       days_since_lockdown_min_yearcurrent_post + 
-       days_since_lockdown_min_yearcurrent_post_X_year2020 | mm_dd | 0 | 0, data = gtrends_df_i)
-
-gtrends_df_i$mm_dd
-
-
-head(gtrends_cases_df)
-
-
-head(gtrends_cases_df)
-
-
-
-gtrends_cases_df$geo_name <- gtrends_cases_df$geo %>% countrycode(origin = "iso2c", destination = "iso.name.en")
-
-gtrends_cases_df <- gtrends_cases_df %>%
-  mutate(days_since_lockdown_min_fact = factor(days_since_lockdown_min),
-         days_since_lockdown_min_fact = days_since_lockdown_min_fact %>% relevel(ref = "-1")) 
-
-# 2.1: Before/After Locdown Average --------------------------------------------
-lockdown_ba_df <- gtrends_cases_df %>%
-  dplyr::filter(abs(days_since_lockdown_min) <= 31) %>%
-  mutate(post_lockdown = (days_since_lockdown_min >= 0)) %>%
-  group_by(geo, keyword_en, post_lockdown) %>%
-  summarise(hits_ma7 = mean(hits_ma7)) %>% 
-  pivot_wider(names_from = post_lockdown,
-              values_from = hits_ma7) %>%
-  dplyr::rename(pre_lockdown = "FALSE",
-                post_lockdown = "TRUE") %>%
-  dplyr::mutate(change = post_lockdown - pre_lockdown,
-                per_change = ((post_lockdown - pre_lockdown) / pre_lockdown) * 100,
-                per_change = case_when(pre_lockdown == post_lockdown ~ 0,
-                                       TRUE ~ per_change)) %>%
-  ungroup()
-
-lockdown_ba_keyavg <- lockdown_ba_df %>%
-  group_by(keyword_en) %>%
-  summarise(change     = mean(change, na.rm=T),
-            per_change = mean(per_change, na.rm=T))
-
-
-
-
-# Event Study: All Countries ---------------------------------------------------
-make_es_figure <- function(keyword, df){
+run_reg <- function(keyword_i, region_i){
+  print(keyword_i)
   
-  df <- gtrends_cases_df %>%
-    filter(keyword_en %in% !!keyword,
-           abs(days_since_lockdown_min) <= 31) 
-  
-  if(nrow(df) > 0){
-    
-    title <- paste0("Search Term: ",
-                    keyword, "; N Countries: ",
-                    length(unique(df$geo)))
-    
-    data_lm <- df %>%
-      felm(hits_ma7 ~ days_since_lockdown_min_fact | geo | 0 | 0, data = .) %>%
-      lm_post_confint_tidy() %>%
-      filter(variable != "(Intercept)") %>%
-      mutate(variable = variable %>%
-               str_replace_all("days_since_lockdown_min_fact", "") %>%
-               as.numeric()) 
-    
-    p <- data_lm %>%
-      ggplot(aes(x = variable,
-                 y = b,
-                 ymin = p025,
-                 ymax = p975)) +
-      geom_vline(xintercept = 0,
-                 color = "red") +
-      geom_point() +
-      geom_linerange() +
-      labs(x = "Days Since Lockdown",
-           y = "Coef +/- 95% CI",
-           title = title) +
-      theme_minimal()
-  } else{
-    p <- ggplot()
+  df_i <- gtrends_df[gtrends_df$keyword_en %in% keyword_i,]
+  if(region_i != "global"){
+    df_i <- df_i[df_i$wb_region %in% region_i,]
   }
   
-  return(p)
+  felm(hits_ma7 ~ year2020 + 
+         days_since_lockdown_min_yearcurrent_post + 
+         days_since_lockdown_min_yearcurrent_post_X_year2020 | mm_dd | 0 | 0, 
+       data = gtrends_df[gtrends_df$keyword_en %in% keyword_i,]) %>%
+    lm_post_confint_tidy() %>%
+    dplyr::mutate(keyword = keyword_i,
+                  region = region_i)
 }
 
-for(keyword_i in unique(gtrends_cases_df$keyword_en)){
-  p <- make_es_figure(keyword_i, gtrends_cases_df)
-  ggsave(p, filename = file.path("~/Desktop/es", paste0(keyword_i, ".png")),
-         height = 3.5, width = 5.5)
-}
+wb_regions_all <- gtrends_df$wb_region %>% unique()
+wb_regions_all <- wb_regions_all[!is.na(wb_regions_all)]
 
-# Example Countries: Boredom ---------------------------------------------------
-p <- gtrends_cases_df %>%
-  filter(geo_name %in% c("United States of America (the)",
-                         "Spain",
-                         "India",
-                         "Angola",
-                         "South Africa",
-                         "Peru",
-                         "Nigeria",
-                         "France",
-                         "Ghana",
-                         "Kenya",
-                         "Colombia",
-                         "Uganda",
-                         "United Kingdom",
-                         "Australia",
-                         "Botswana",
-                         "New Zealand",
-                         "Trinidad and Tobago",
-                         "Venezuela (Bolivarian Republic of)",
-                         "Dominican Republic (the)",
-                         "Zimbabwe")) %>%
-  mutate(geo_name = case_when(geo_name %in% "Venezuela (Bolivarian Republic of)" ~ "Venezuela",
-                              geo_name %in% "Dominican Republic (the)" ~ "Dominican Republic",
-                              geo_name %in% "United States of America (the)" ~ "USA",
-                              TRUE ~ geo_name)) %>%
-  filter(keyword_en %in% c("boredom")) %>%
-  filter(abs(days_since_lockdown_min) <= 31) %>%
-  ggplot() +
-  geom_vline(xintercept = 0,
-             color = "red") +
-  geom_line(aes(x = days_since_lockdown_min,
-                y = hits_ma7)) +
-  facet_wrap(~geo_name,
-             scales = "free_y") +
-  labs(x = "Days Since Lockdown",
-       y = "Search Interest") +
-  theme_minimal() +
-  theme(strip.text = element_text(face = "bold", size =7))
-ggsave(p, filename = file.path("~/Desktop/boredom.png"),
-       height = 5, width = 7)
+global_results_df <- map_df(keywords_en_use, run_reg, "global")
+region_results_df <- map_df(wb_regions_all, function(region_i){
+  map_df(keywords_en_use, run_reg, region_i)
+})
 
-# Example Countries: Unemployment ---------------------------------------------------
-p <- gtrends_cases_df %>%
-  filter(geo_name %in% c("United States of America (the)",
-                         "Spain",
-                         "Argentina",
-                         "Bahamas (the)",
-                         "Belize",
-                         "Bulgaria",
-                         "Canada",
-                         "Colombia",
-                         "Chile",
-                         "Iceland",
-                         "Iran (Islamic Republic of)",
-                         "Italy",
-                         "Luxembourg",
-                         "Malta",
-                         "North Macedonia",
-                         "Poland",
-                         "Russian Federation (the)",
-                         "Thailand",
-                         "United Kingdom",
-                         "Uruguay"
-  )) %>%
-  mutate(geo_name = case_when(geo_name %in% "Venezuela (Bolivarian Republic of)" ~ "Venezuela",
-                              geo_name %in% "Bahamas (the)" ~ "Bahamas",
-                              geo_name %in% "Russian Federation (the)" ~ "Russia",
-                              geo_name %in% "Iran (Islamic Republic of)" ~ "Iran",
-                              geo_name %in% "Dominican Republic (the)" ~ "Dominican Republic",
-                              geo_name %in% "United States of America (the)" ~ "USA",
-                              TRUE ~ geo_name)) %>%
-  filter(keyword_en %in% c("unemployment")) %>%
-  filter(abs(days_since_lockdown_min) <= 31) %>%
-  ggplot() +
-  geom_vline(xintercept = 0,
-             color = "red") +
-  geom_line(aes(x = days_since_lockdown_min,
-                y = hits_ma7)) +
-  facet_wrap(~geo_name,
-             scales = "free_y") +
-  labs(x = "Days Since Lockdown",
-       y = "Search Interest") +
-  theme_minimal() +
-  theme(strip.text = element_text(face = "bold", size =7))
-ggsave(p, filename = file.path("~/Desktop/unemployment.png"),
-       height = 5, width = 7)
+results_df <- bind_rows(global_results_df, region_results_df)
 
 
 
-p <- gtrends_cases_df %>%
-  mutate(geo_name = case_when(geo_name %in% "Venezuela (Bolivarian Republic of)" ~ "Venezuela",
-                              geo_name %in% "Dominican Republic (the)" ~ "Dominican Republic",
-                              geo_name %in% "United States of America (the)" ~ "USA",
-                              TRUE ~ geo_name)) %>%
-  filter(keyword_en %in% c("unemployment")) %>%
-  filter(abs(days_since_lockdown_min) <= 31) %>%
-  ggplot() +
-  geom_vline(xintercept = 0,
-             color = "red") +
-  geom_line(aes(x = days_since_lockdown_min,
-                y = hits_ma7)) +
-  facet_wrap(~geo_name,
-             scales = "free_y") +
-  labs(x = "Days Since Lockdown",
-       y = "Search Interest") +
-  theme_minimal() +
-  theme(strip.text = element_text(face = "bold", size =7))
-ggsave(p, filename = file.path("~/Desktop/unemployment_l.png"),
-       height = 20, width = 20)
+
+
